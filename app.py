f01fbb25f65588d1fd01db5a1694c0c0867ca743
Cathagiani <crodriguezg@fi.uba.ar>
2025-05-04 22:37:01 -0300

    cambios en todo

--- app.py
+++ app.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/comision', methods=['POST'])
-def comision():
-    nombre = request.form.get('nombre')
-    email = request.form.get('email')
-    detalle = request.form.get('detalle')
-    print(f"Nuevo pedido: {nombre}, {email}, {detalle}")
-    return redirect(url_for('home'))
-
 if __name__ == "__main__":
     app.run(debug=True)
